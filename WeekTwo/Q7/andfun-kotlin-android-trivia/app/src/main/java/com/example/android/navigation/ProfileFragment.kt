e4a13c966cd21f59cfc1873e493adcbb08c1d349
James <der.zehlenteufel@gmail.com>
2020-04-22 17:31:40 +0800

    Add data binding to profile fragment

--- WeekTwo/Q7/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/ProfileFragment.kt
+++ WeekTwo/Q7/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentProfileBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,8 +15,10 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val binding : FragmentProfileBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_profile,container,false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return binding.root
     }
 
 }
